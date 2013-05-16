7b4514c481bbf95063d84b2a942a3f152ef58716
Thoriso Ralefu <tralefu@gmail.com>
2013-05-16 12:22:40 +0200

    - Added the Dispute EJB's, TO's, WebServices, Webservices Clients, Client Beans and the Dispute UI.

--- src/main/java/org/sola/common/messaging/ClientMessage.java
+++ src/main/java/org/sola/common/messaging/ClientMessage.java
@@ -52,6 +52,7 @@ public class ClientMessage {
     private static final String BR = MSG_PREFIX + "br";
     private static final String PROGRESSMSG = MSG_PREFIX + "prgs";
     private static final String BULK_OPERATIONS = MSG_PREFIX + "_bulk_operations_";
+    private static final String DISPUTE = MSG_PREFIX + "dispute";
     // <editor-fold defaultstate="collapsed" desc="Test Messages">  
     /**
      * clitest001 - Unit Test Message
@@ -773,6 +774,15 @@ public class ClientMessage {
      * clibaunit026 - Enter lease rejection reason
      */
     public static final String BAUNIT_LEASE_REJECTION_REASON_TITLE = BAUNIT + "026";
+    
+     /**
+     * clidispute001 - Property has been saved successfully.
+     */
+    public static final String DISPUTE_SAVED = DISPUTE + "001";
+    /**
+     * clidispute002 - Failed to save property.
+     */
+    public static final String DISPUTE_SAVE_FAILED = DISPUTE + "002";
     
     // General error messages
     /**
