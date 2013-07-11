c05d960bc7b6484b3ac767342e65f75f047fc5b2
Bulara <mbulara@gmail.com>
2013-07-11 17:46:49 +0200

    Introduced party legal status type in the Clients desktop application

--- src/main/java/org/sola/common/messaging/ClientMessage.java
+++ src/main/java/org/sola/common/messaging/ClientMessage.java
@@ -270,10 +270,15 @@ public class ClientMessage {
     public static final String CHECK_BEANNOTNULL_BRVALTARGETTYPE = CHECK + "059";
     public static final String CHECK_GENERICNULL_OBJECT = CHECK + "060";
     public static final String CHECK_GENERICDUPL_OBJECT = CHECK + "061";
+    
     /**
     * clichck204 - Gender cannot be null.
     */
     public static final String CHECK_NOTNULL_GENDER = CHECK + "204";   
+    /**
+    * clichck204 - Gender cannot be null.
+    */
+    public static final String CHECK_NOTNULL_LEGAL_STATUS = CHECK + "205";   
      
     /**
      * clichck062 - Number of services should be greater than 0.
